80200c2860ff5ed1fa4a1cea6d8bd136ae4c22f9
Raphaël Monnard <raphael@monnard.email>
2023-06-11 15:52:42 +0200

    add display and switch button

--- software/receiver/receiver.ino
+++ software/receiver/receiver.ino
@@ -6,6 +6,16 @@
 #include <RH_ASK.h>
 RH_ASK driver;
 
+// Init display
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
 // Init FastLED
 #include<FastLED.h>
 
@@ -34,6 +44,14 @@ struct DataPacket {
 int mic;
 int micSensitivity = 512;
 
+int brightness = 32;
+
+const int BTN_1 = 12;  // Pin connected to the reference button
+const int BTN_2 = 13;  // Pin connected to the button
+int buttonState = HIGH;    // Current state of the button
+int lastButtonState = HIGH;  // Previous state of the button
+int mode = 0;  // Current mode (0: brightness, 1: mic sensitivity, 2: spectacle)
+
 void setLEDs(CRGB* leds, int numLeds, int scaledValue, CRGB color) {
   for (int i = 0; i < numLeds; i++) {
     if (i < scaledValue) {
@@ -42,8 +60,6 @@ void setLEDs(CRGB* leds, int numLeds, int scaledValue, CRGB color) {
       leds[i] = CRGB::Black;
     }
   }
-  int brightness = map(analogRead(A1), 0, 1023, 0, 255);
-  FastLED.setBrightness(brightness);
   FastLED.show();
 }
 
@@ -55,38 +71,91 @@ void setupLeds(CRGB* leds, int numLeds) {
 
 void setup()
 {
-    Serial.begin(9600);	// Debugging only
-    if (!driver.init())
-         Serial.println("init failed");
+  Serial.begin(9600);	// Debugging only
+  if (!driver.init())
+        Serial.println("init failed");
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.display();
+  delay(500);
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);    
 
   setupLeds<DATA_PIN_CE>(leds_CE, NUM_LEDS_CE);
   setupLeds<DATA_PIN_JB>(leds_JB, NUM_LEDS_JB);
   setupLeds<DATA_PIN_LB>(leds_LB, NUM_LEDS_LB);
   setupLeds<DATA_PIN_SA>(leds_SA, NUM_LEDS_SA);
+
+  pinMode(BTN_1, INPUT_PULLUP);
+  pinMode(BTN_2, INPUT_PULLUP);
 }
 
 void loop()
 {
-  mic = map(analogRead(A0), 0, micSensitivity, 0, 100);
-  uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
-  uint8_t buflen = sizeof(buf);
-  if (driver.recv(buf, &buflen)) {
-    Serial.println("Message received !");
-    // Message received
-    DataPacket packet;
-    if (buflen == sizeof(packet)) {
-      memcpy(&packet, buf, sizeof(packet));
-      // Process the received packet
-      displayPacket(packet);
-
-      // Get potentiometer value
-      int scaledValue = packet.value;
-      setLEDs(leds_CE, NUM_LEDS_CE, scaledValue, CRGB::Green);
-      setLEDs(leds_JB, NUM_LEDS_JB, scaledValue, CRGB::Blue);
-      setLEDs(leds_LB, NUM_LEDS_LB, scaledValue, CRGB::Red);
-      setLEDs(leds_SA, NUM_LEDS_SA, scaledValue, CRGB::Yellow);
+ // Read the button state
+  buttonState = digitalRead(BTN_2);
+  // Check if the button state has changed
+  if (buttonState != lastButtonState) {
+    // Check if the button is pressed (LOW state)
+    if (buttonState == LOW) {
+      // Increment the mode and wrap around if necessary
+      mode = (mode + 1) % 3;
     }
   }
+  
+  // Perform mode-specific actions based on the current mode
+  switch (mode) {
+    case 0:
+      // Brightness mode
+      updateDisplay("Brightness");
+      brightness = map(analogRead(A1), 0, 1023, 0, 255);
+      FastLED.setBrightness(brightness);
+      FastLED.show();
+      break;
+    case 1:
+      // Mic sensitivity mode
+      updateDisplay("Mic Sensitivity");
+      break;
+    case 2:
+      // Spectacle mode
+      updateDisplay("Spectacle");
+      mic = map(analogRead(A0), 0, micSensitivity, 0, 100);
+      uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
+      uint8_t buflen = sizeof(buf);
+      if (driver.recv(buf, &buflen)) {
+        Serial.println("Message received !");
+        // Message received
+        DataPacket packet;
+        if (buflen == sizeof(packet)) {
+          memcpy(&packet, buf, sizeof(packet));
+          // Process the received packet
+          displayPacket(packet);
+
+          // Get potentiometer value
+          int scaledValue = packet.value;
+          setLEDs(leds_CE, NUM_LEDS_CE, scaledValue, CRGB::Green);
+          setLEDs(leds_JB, NUM_LEDS_JB, scaledValue, CRGB::Blue);
+          setLEDs(leds_LB, NUM_LEDS_LB, scaledValue, CRGB::Red);
+          setLEDs(leds_SA, NUM_LEDS_SA, scaledValue, CRGB::Yellow);
+        }
+      }
+      break;
+  }
+  // Update the last button state
+  lastButtonState = buttonState;
+}
+
+void updateDisplay(String text) {
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.println(text);
+  display.display();  
 }
 
 void displayPacket(DataPacket packet) {
