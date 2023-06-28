fa926cb80c3544ac7567607c55f6d8afb77a2b0d
Raphaël Monnard <raphael@monnard.email>
2023-06-28 18:47:22 +0200

    update void loop for LED integrations

--- software/receiver/receiver.ino
+++ software/receiver/receiver.ino
@@ -20,19 +20,19 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 #include<FastLED.h>
 
 #define NUM_LEDS_CE 20
-#define DATA_PIN_CE 4
+#define DATA_PIN_CE 7
 CRGB leds_CE[NUM_LEDS_CE];
 
 #define NUM_LEDS_JB 20
-#define DATA_PIN_JB 5
+#define DATA_PIN_JB 6
 CRGB leds_JB[NUM_LEDS_JB];
 
 #define NUM_LEDS_LB 20
-#define DATA_PIN_LB 6
+#define DATA_PIN_LB 5
 CRGB leds_LB[NUM_LEDS_LB];
 
 #define NUM_LEDS_SA 20
-#define DATA_PIN_SA 7
+#define DATA_PIN_SA 4
 CRGB leds_SA[NUM_LEDS_SA];
 
 struct DataPacket {
@@ -42,7 +42,12 @@ struct DataPacket {
 };
 
 int scaledValue;
+
 int scaledValue_CE = 1;
+int scaledValue_JB = 1;
+int scaledValue_LB = 1;
+int scaledValue_SA = 1;
+
 int channel = 0; 
 int value = 100;
 int value_CE = 100;
@@ -51,7 +56,7 @@ int max = 100;
 int mic = 0;
 int micSensitivity = 1023;
 
-int brightness = 32;
+int brightness;
 
 void updateDisplay(String text) {
   display.clearDisplay();
@@ -75,7 +80,6 @@ void setLEDs(CRGB* leds, int numLeds, int scaledValue, CRGB color) {
       leds[i] = CRGB::Black;
     }
   }
-  FastLED.show();
 }
 
 template <uint8_t DATA_PIN>
@@ -110,69 +114,63 @@ void setup()
 
 void loop()
 {
- // Read the button state
-  buttonState = digitalRead(BTN_2);
-  // Check if the button state has changed
-  if (buttonState != lastButtonState) {
-    // Check if the button is pressed (LOW state)
-    if (buttonState == LOW) {
-      // Increment the mode and wrap around if necessary
-      mode = (mode + 1) % 3;
+  updateDisplay("Spectacle");
+
+  uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
+  uint8_t buflen = sizeof(buf);
+  DataPacket packet;
+  if (driver.available()) {
+    uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
+    uint8_t buflen = sizeof(buf);
+    if (driver.recv(buf, &buflen)) {
+      Serial.println("Message received !");
+      // Message received
+      if (buflen == sizeof(packet)) {
+        memcpy(&packet, buf, sizeof(packet));
+        // Process the received packet
+        value = packet.value;
+        channel = packet.channel;
+        micSensitivity = packet.micSensitivity;
+        //displayPacket(packet);
+      }
     }
   }
   
-  // Perform mode-specific actions based on the current mode
-  switch (mode) {
-    case 0:
-      // Brightness mode
-      updateDisplay("Brightness");
-      brightness = map(analogRead(A1), 0, 1023, 0, 255);
-      FastLED.setBrightness(brightness);
-      FastLED.show();
-      break;
-    case 1:
-      // Mic sensitivity mode
-      updateDisplay("Mic Sensitivity");
-      break;
-    case 2:
-      // Spectacle mode
-      updateDisplay("Spectacle");
-      mic = map(analogRead(A0), 0, micSensitivity, 0, 100);
-      uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
-      uint8_t buflen = sizeof(buf);
-      if (driver.recv(buf, &buflen)) {
-        Serial.println("Message received !");
-        // Message received
-        DataPacket packet;
-        if (buflen == sizeof(packet)) {
-          memcpy(&packet, buf, sizeof(packet));
-          // Process the received packet
-          displayPacket(packet);
+  int8_t value_CE = pgm_read_word(&(params[channel][0]));
+  int8_t value_JB = pgm_read_word(&(params[channel][1]));
+  int8_t value_LB = pgm_read_word(&(params[channel][2]));
+  int8_t value_SA = pgm_read_word(&(params[channel][3]));
+   
+  int8_t weight = pgm_read_word(&(params[channel][4]));
+  int8_t max = pgm_read_word(&(params[channel][5]));
+  mic = map(analogRead(A0), 0, micSensitivity, 0, 100);
 
-          // Get potentiometer value
-          int scaledValue = packet.value;
-          setLEDs(leds_CE, NUM_LEDS_CE, scaledValue, CRGB::Green);
-          setLEDs(leds_JB, NUM_LEDS_JB, scaledValue, CRGB::Blue);
-          setLEDs(leds_LB, NUM_LEDS_LB, scaledValue, CRGB::Red);
-          setLEDs(leds_SA, NUM_LEDS_SA, scaledValue, CRGB::Yellow);
-        }
-      }
-      break;
+  scaledValue = (weight * value / 100 + (100 - weight) * mic / 100 ) * max / 100; // valeur relative
+  scaledValue_CE = map(scaledValue, 0, 100, 0, NUM_LEDS_CE) * value_CE / 100; // valeur absolue
+  scaledValue_JB = map(scaledValue, 0, 100, 0, NUM_LEDS_JB) * value_JB / 100; // valeur absolue
+  scaledValue_LB = map(scaledValue, 0, 100, 0, NUM_LEDS_LB) * value_LB / 100; // valeur absolue
+  scaledValue_SA = map(scaledValue, 0, 100, 0, NUM_LEDS_SA) * value_SA / 100; // valeur absolue
+
+  // Serial.print("Channel: ");
+  // Serial.print(channel);
+  // Serial.print(" - weight: ");
+  // Serial.print(weight);
+  // Serial.print(" - value: ");
+  // Serial.print(value);
+  // Serial.print(" - mic: ");
+  // Serial.print(mic);
+  // Serial.print(" - scaledValue: ");
+  // Serial.print(scaledValue);
+  // Serial.print(" - scaledValue CE: ");
+  // Serial.println(scaledValue_CE);
+
+  setLEDs(leds_CE, NUM_LEDS_CE, scaledValue_CE, CRGB::Green);
+  setLEDs(leds_JB, NUM_LEDS_JB, scaledValue, CRGB::Blue);
+  setLEDs(leds_LB, NUM_LEDS_LB, scaledValue, CRGB::Red);
+  setLEDs(leds_SA, NUM_LEDS_SA, scaledValue, CRGB::Yellow);
+
+  brightness = map(analogRead(A1), 0, 1023, 0, 255);
+  FastLED.setBrightness(brightness); // Set the initial brightness  
+  
+  FastLED.show();
   }
-  // Update the last button state
-  lastButtonState = buttonState;
-}
-
-void updateDisplay(String text) {
-  display.clearDisplay();
-  display.setCursor(0, 0);
-  display.println(text);
-  display.display();  
-}
-
-void displayPacket(DataPacket packet) {
-  Serial.print("Received Packet - channel: ");
-  Serial.print(packet.channel);
-  Serial.print(", Value: ");
-  Serial.println(packet.value);
-}
