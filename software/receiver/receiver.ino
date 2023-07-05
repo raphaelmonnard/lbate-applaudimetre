0ebd2574a1cd52728027e98fcf99cf3b7a922c82
Raphaël Monnard <raphael@monnard.email>
2023-07-05 15:09:21 +0200

    add cusotm color, remove display, clean code

--- software/receiver/receiver.ino
+++ software/receiver/receiver.ino
@@ -1,38 +1,37 @@
-#include <SPI.h> // Not actualy used but needed to compile
+#include <SPI.h> // Not actually used but needed to compile
 #include <avr/pgmspace.h>
 #include "spectacle.h"
-
-// Init RadioHead for transmission
 #include <RH_ASK.h>
-RH_ASK driver;
+// #include "SSD1306Ascii.h"
+// #include "SSD1306AsciiAvrI2c.h"
+#include <FastLED.h>
 
-// Init display
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+// // 0X3C+SA0 - 0x3C or 0x3D
+// #define I2C_ADDRESS 0x3C
 
-// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-#define OLED_RESET -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+// // Define proper RST_PIN if required.
+// #define RST_PIN -1
 
-// Init FastLED
-#include<FastLED.h>
+// SSD1306AsciiAvrI2c oled;
 
-#define NUM_LEDS_CE 20
+#define NUM_LEDS_CE 30
 #define DATA_PIN_CE 7
+#define COLOR_CE 0x0EA500
 CRGB leds_CE[NUM_LEDS_CE];
 
-#define NUM_LEDS_JB 20
+#define NUM_LEDS_JB 30
 #define DATA_PIN_JB 6
+#define COLOR_JB 0x0045E5
 CRGB leds_JB[NUM_LEDS_JB];
 
-#define NUM_LEDS_LB 20
+#define NUM_LEDS_LB 28
 #define DATA_PIN_LB 5
+#define COLOR_LB 0xD200AB
 CRGB leds_LB[NUM_LEDS_LB];
 
-#define NUM_LEDS_SA 20
+#define NUM_LEDS_SA 30
 #define DATA_PIN_SA 4
+#define COLOR_SA 0xE5DD00
 CRGB leds_SA[NUM_LEDS_SA];
 
 struct DataPacket {
@@ -41,124 +40,107 @@ struct DataPacket {
   int micSensitivity = 0;
 };
 
-int scaledValue;
-int scaledValue_CE = 1;
-int channel = 0; 
+// Adafruit_SSD1306 display(128, 32, &Wire, -1);
+RH_ASK driver;
+DataPacket packet;
+int channel = 0;
 int value = 100;
-int value_CE = 100;
 int weight = 100;
 int max = 100;
 int mic = 0;
 int micSensitivity = 1023;
-
 int brightness = 32;
+double temperature;
 
-void updateDisplay(String text) {
-  display.clearDisplay();
-  display.setCursor(0, 0);
-  display.println(text);
-  display.display();  
-}
-
-void displayPacket(DataPacket packet) {
-  Serial.print("Received Packet - channel: ");
-  Serial.print(packet.channel);
-  Serial.print(", Value: ");
-  Serial.println(packet.value);
-}
+// void updateDisplay(const String& text) {
+//   display.clearDisplay();
+//   display.setCursor(0, 0);
+//   display.println(text);
+//   display.display();
+// }
 
 void setLEDs(CRGB* leds, int numLeds, int scaledValue, CRGB color) {
   for (int i = 0; i < numLeds; i++) {
-    if (i < scaledValue) {
-      leds[i] = color;
-    } else {
-      leds[i] = CRGB::Black;
-    }
+    leds[i] = (i < scaledValue) ? color : CRGB::Black;
   }
 }
 
-template <uint8_t DATA_PIN>
-void setupLeds(CRGB* leds, int numLeds) {
+template <uint8_t DATA_PIN, CRGB* leds, int numLeds>
+void setupLeds() {
   FastLED.addLeds<WS2812, DATA_PIN, RGB>(leds, numLeds);
   pinMode(DATA_PIN, OUTPUT);
 }
 
-void setup()
-{
-  Serial.begin(9600);	// Debugging only
+void setup() {
+  Serial.begin(9600);
   if (!driver.init())
-        Serial.println("init failed");
+    Serial.println("init failed");
 
-  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
-  }
-  // Show initial display buffer contents on the screen --
-  // the library initializes this with an Adafruit splash screen.
-  display.display();
-  delay(500);
-  display.setTextSize(1);
-  display.setTextColor(SSD1306_WHITE);    
+  // #if RST_PIN >= 0
+  //   oled.begin(&Adafruit128x32, I2C_ADDRESS, RST_PIN);
+  // #else // RST_PIN >= 0
+  //   oled.begin(&Adafruit128x32, I2C_ADDRESS);
+  // #endif // RST_PIN >= 0
+  //   // Call oled.setI2cClock(frequency) to change from the default frequency.
 
-  setupLeds<DATA_PIN_CE>(leds_CE, NUM_LEDS_CE);
-  setupLeds<DATA_PIN_JB>(leds_JB, NUM_LEDS_JB);
-  setupLeds<DATA_PIN_LB>(leds_LB, NUM_LEDS_LB);
-  setupLeds<DATA_PIN_SA>(leds_SA, NUM_LEDS_SA);
+  // oled.setFont(Adafruit5x7);
+
+  // oled.clear();
+
+  // // first row
+  // oled.println("set1X test");
+
+  // if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+  //   Serial.println(F("SSD1306 allocation failed"));
+  //   for (;;) {}
+  // }
+  // display.display();
+  // delay(500);
+  // display.setTextSize(1);
+  // display.setTextColor(SSD1306_WHITE);
+
+  setupLeds<DATA_PIN_CE, leds_CE, NUM_LEDS_CE>();
+  setupLeds<DATA_PIN_JB, leds_JB, NUM_LEDS_JB>();
+  setupLeds<DATA_PIN_LB, leds_LB, NUM_LEDS_LB>();
+  setupLeds<DATA_PIN_SA, leds_SA, NUM_LEDS_SA>();
 }
 
-void loop()
-{
-  updateDisplay("Spectacle");
+void loop() {
+  // updateDisplay("Spectacle");
+  Serial.println("start loop");
 
-  uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
-  uint8_t buflen = sizeof(buf);
-  DataPacket packet;
   if (driver.available()) {
     uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
     uint8_t buflen = sizeof(buf);
-    if (driver.recv(buf, &buflen)) {
-      Serial.println("Message received !");
-      // Message received
-      if (buflen == sizeof(packet)) {
-        memcpy(&packet, buf, sizeof(packet));
-        // Process the received packet
-        value = packet.value;
-        channel = packet.channel;
-        micSensitivity = packet.micSensitivity;
-        //displayPacket(packet);
-      }
+    if (driver.recv(buf, &buflen) && buflen == sizeof(packet)) {
+      memcpy(&packet, buf, sizeof(packet));
+      value = packet.value;
+      channel = packet.channel;
+      micSensitivity = packet.micSensitivity;
     }
   }
-  
+
   int8_t value_CE = pgm_read_word(&(params[channel][0]));
+  int8_t value_JB = pgm_read_word(&(params[channel][1]));
+  int8_t value_LB = pgm_read_word(&(params[channel][2]));
+  int8_t value_SA = pgm_read_word(&(params[channel][3]));
   int8_t weight = pgm_read_word(&(params[channel][4]));
   int8_t max = pgm_read_word(&(params[channel][5]));
   mic = map(analogRead(A0), 0, micSensitivity, 0, 100);
 
-  scaledValue = (weight * value / 100 + (100 - weight) * mic / 100 ) * max / 100; // valeur relative
-  scaledValue_CE = map(scaledValue, 0, 100, 0, NUM_LEDS_CE) * value_CE / 100; // valeur absolue
+  int scaledValue = (weight * value / 100 + (100 - weight) * mic / 100) * max / 100;
+  int scaledValue_CE = map(scaledValue, 0, 100, 0, NUM_LEDS_CE) * value_CE / 100;
+  int scaledValue_JB = map(scaledValue, 0, 100, 0, NUM_LEDS_JB) * value_JB / 100;
+  int scaledValue_LB = map(scaledValue, 0, 100, 0, NUM_LEDS_LB) * value_LB / 100;
+  int scaledValue_SA = map(scaledValue, 0, 100, 0, NUM_LEDS_SA) * value_SA / 100;
 
-  // Serial.print("Channel: ");
-  // Serial.print(channel);
-  // Serial.print(" - weight: ");
-  // Serial.print(weight);
-  // Serial.print(" - value: ");
-  // Serial.print(value);
-  // Serial.print(" - mic: ");
-  // Serial.print(mic);
-  // Serial.print(" - scaledValue: ");
-  // Serial.print(scaledValue);
-  // Serial.print(" - scaledValue CE: ");
-  // Serial.println(scaledValue_CE);
-
-  setLEDs(leds_CE, NUM_LEDS_CE, scaledValue_CE, CRGB::Green);
-  setLEDs(leds_JB, NUM_LEDS_JB, scaledValue, CRGB::Blue);
-  setLEDs(leds_LB, NUM_LEDS_LB, scaledValue, CRGB::Red);
-  setLEDs(leds_SA, NUM_LEDS_SA, scaledValue, CRGB::Yellow);
+  setLEDs(leds_CE, NUM_LEDS_CE, scaledValue_CE, COLOR_CE);
+  setLEDs(leds_JB, NUM_LEDS_JB, scaledValue, COLOR_JB);
+  setLEDs(leds_LB, NUM_LEDS_LB, scaledValue, COLOR_LB);
+  setLEDs(leds_SA, NUM_LEDS_SA, scaledValue, COLOR_SA);
 
   brightness = map(analogRead(A1), 0, 1023, 0, 255);
-  FastLED.setBrightness(brightness); // Set the initial brightness  
-  
+  FastLED.setBrightness(brightness);
+
   FastLED.show();
-  }
+}
