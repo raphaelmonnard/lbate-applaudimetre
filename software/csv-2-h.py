0fbcd8fdbb03cc53b92613fd1a9ad6cc1675d497
Raphaël Monnard <raphael@monnard.email>
2023-07-08 15:24:14 +0200

    remove Bool array

--- software/csv-2-h.py
+++ software/csv-2-h.py
@@ -18,20 +18,14 @@ with open(csv_file, 'r') as file:
     next(reader)  # Skip the header row
     
     for row in reader:
-        bool_array.append([value.lower() for value in row[3:7]])
-        int_array.append(row[7:9])
+        int_array.append(row[3:9])
         string_array.append([row[1]])
 
 # Generate the header file content
 header_content = ""
 
-# Generate the bool table
-header_content += "const bool stripes[{}][{}] = {{\n".format(len(bool_array), len(bool_array[0]))
-header_content += ",\n".join("    {" + ",".join(str(value) for value in row) + "}" for row in bool_array)
-header_content += "\n};\n\n"
-
 # Generate the int table
-header_content += "const int params[{}][{}] = {{\n".format(len(int_array), len(int_array[0]))
+header_content += "const int params[{}][{}] PROGMEM = {{\n".format(len(int_array), len(int_array[0]))
 header_content += ",\n".join("    {" + ",".join(str(value) for value in row) + "}" for row in int_array)
 header_content += "\n};\n\n"
 
